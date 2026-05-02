cbdb145ce7457843bfd968b408cd8975ba300d25
ninadbangal83 <bangalninad83@gmail.com>
2026-05-02 19:02:14 +0530

    refactor: simplify PrismaService constructor

--- apps/api-gateway/src/infrastructure/database/prisma/prisma.service.ts
+++ apps/api-gateway/src/infrastructure/database/prisma/prisma.service.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  constructor(private readonly configService: ConfigService) {
+  constructor(configService: ConfigService) {
     super({
       log: configService.get<string>('NODE_ENV') === 'development' ? ['query', 'info', 'warn', 'error'] : ['error'],
     });
